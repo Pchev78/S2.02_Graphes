5e98d7c6ac5471dc87ec6d4245c0d24947f7e188
ysy15 <128068301+ysy15@users.noreply.github.com>
2023-04-04 00:14:47 +0200

    Update Arc.java

--- src/Arc/Arc.java
+++ src/Arc/Arc.java
@@ -1,49 +1,53 @@
 package Arc;
 
-public class Arc{
+
+public class Arc implements Comparable<Arc>{
     private String source;
     private String destination;
     private int valuation;
-
-
+    
+    
     public Arc(String noeud) {
-        source = noeud;
-        destination = "";
-        valuation = 0;
+    	source = noeud;
+    	destination = "";
+    	valuation = 0;
     }
-
+    
     public Arc(String source, String destination, int valuation) {
-        this.source = source;
-        this.destination = destination;
-        this.valuation = valuation;
+    	this.source = source;
+    	this.destination = destination;
+    	this.valuation = valuation;
     }
-
+    
     public String getSource() {
-        return source;
+    	return source;
     }
-
+    
     public String getDestination() {
-        return destination;
+    	return destination;
     }
-
+    
     public int getValuation() {
-        return valuation;
+    	return valuation;
     }
-
+    
     public boolean equals(Arc ArcCompare) {
-        return source.equals(ArcCompare.source) && destination.equals(ArcCompare.destination);
+    	return source.equals(ArcCompare.source) && destination.equals(ArcCompare.destination);
     }
-
+    
     public String toString() {
-        if (destination == "") {
-            String s = source + ":";
-            return s;
-        }
-        String s = source + "-" + destination + "(" + valuation + ")";
-        return s;
+    	if (destination == "") {
+    		String s = source + ":";
+    		return s;
+    	}
+    	String s = source + "-" + destination + "(" + valuation + ")";
+    	return s;
     }
 
-//    private int compareTo(SAE.Arc.Arc ArcCompare) {
-//    	return source.compareTo(ArcCompare.getSource());
-//    }
+	@Override
+	public int compareTo(Arc o) {
+		return source.compareTo(o.getSource());
+	}
+    
+   
 }
