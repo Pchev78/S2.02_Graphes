c7f070431baf4ed017d915a26c6142fd73ba78ab
ysy15 <128068301+ysy15@users.noreply.github.com>
2023-04-03 13:46:24 +0200

    Update Arc.java

--- Arc.java
+++ Arc.java
@@ -1,19 +1,49 @@
 package SAE;
 
-public class Arc {
+public class Arc{
     private String source;
     private String destination;
     private int valuation;
+    
+    
+    public Arc(String noeud) {
+    	source = noeud;
+    	destination = "";
+    	valuation = 0;
+    }
+    
     public Arc(String source, String destination, int valuation) {
-
+    	this.source = source;
+    	this.destination = destination;
+    	this.valuation = valuation;
     }
-    public void ajouterArc() {
-
+    
+    public String getSource() {
+    	return source;
     }
-    public boolean equals() {
-        //
+    
+    public String getDestination() {
+    	return destination;
     }
+    
+    public int getValuation() {
+    	return valuation;
+    }
+    
+    public boolean equals(Arc ArcCompare) {
+    	return source.equals(ArcCompare.source) && destination.equals(ArcCompare.destination);
+    }
+    
     public String toString() {
-
+    	if (destination == "") {
+    		String s = source + ":";
+    		return s;
+    	}
+    	String s = source + "-" + destination + "(" + valuation + ")";
+    	return s;
     }
+    
+//    private int compareTo(Arc ArcCompare) {
+//    	return source.compareTo(ArcCompare.getSource());
+//    }
 }
