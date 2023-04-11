37b4a2e4efcb416235f3f09c5971fddc6c6a6577
ysy <128068301+ysy15@users.noreply.github.com>
2023-04-11 23:20:03 +0200

    Update GrapheLArcs.java

--- src/representation/GrapheLArcs.java
+++ src/representation/GrapheLArcs.java
@@ -1,24 +1,42 @@
 package representation;
 
+/**
+ * Classe représentant un graphe sous forme d'une liste d'arcs
+ * @author MOGNE Youssouf-A., CHEVILY Pierre, BALASSOUPRAMANIEN Madavan, BANOL MORENO Marcell 101
+ */
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import Arc.Arc;
 
+/** Type de donnée représentant un graphe sous forme d'une liste d'arcs*/
 public class GrapheLArcs implements graphe.IGraphe {
-    // Représenter sous forme de liste d'arcs
-    private List<Arc> arcs;
+    private List<Arc> arcs; //// Liste d'arcs
     
+    /**
+	 * @brief Constructeur qui initialise un graphe représenté via une liste d'arcs
+	 */
     public GrapheLArcs() {
     	arcs = new ArrayList<Arc>();
     }
     
+    
+    /**
+	 * @brief Constructeur qui initialise un graphe à partir d'une chaîne
+	 * @param s : chaîne indiquant les arcs du graphe
+	 */
     public GrapheLArcs(String s) {
     	this();
     	peupler(s);
     }
     
+    
+    /**
+	 * @brief Renvoie tous les sommets du graphe
+	 * @return liste de tous les sommets
+	 */
 	@Override
 	public List<String> getSommets() {
 		List<String> sommets = new ArrayList<String>();
@@ -30,6 +48,12 @@ public class GrapheLArcs implements graphe.IGraphe {
 		return sommets;
 	}
 
+	
+	/**
+	 * @brief Renvoie tous les successeurs d'un sommet
+	 * @param sommet : sommet dont on veut connaître les successeurs
+	 * @return liste des successeurs du sommet
+	 */
 	@Override
 	public List<String> getSucc(String sommet) {
 		List<String> succ = new ArrayList<String>();
@@ -41,6 +65,12 @@ public class GrapheLArcs implements graphe.IGraphe {
 		return succ;
 	}
 
+	/**
+	 * @brief Renvoie la valuation entre deux sommets, -1 s'il n'existe pas d'arc entre les deux sommets
+	 * @param src : sommet source
+	 * @param dest : sommet destination
+	 * @return la valuation entre les deux sommets
+	 */
 	@Override
 	public int getValuation(String src, String dest) {
 		assert(contientSommet(src) && contientSommet(dest));
@@ -52,11 +82,23 @@ public class GrapheLArcs implements graphe.IGraphe {
 		return -1;
 	}
 
+	
+	/**
+	 * @brief Renvoie un booléen indiquant si le graphe contient un sommet ou non
+	 * @param sommet : sommet que l'on veut vérifier
+	 * @return true si le sommet existe dans le graphe, false sinon
+	 */
 	@Override
 	public boolean contientSommet(String sommet) {
 		return getSommets().contains(sommet);
 	}
 
+	/**
+	 * @brief Renvoie un booléen indiquant si le graphe contient un arc ou non
+	 * @param src : sommet source
+	 * @param dest : sommet destination
+	 * @return true si un arc existe entre les deux sommets, false sinon
+	 */
 	@Override
 	public boolean contientArc(String src, String dest) {
 		if (contientSommet(src)) {
@@ -65,14 +107,23 @@ public class GrapheLArcs implements graphe.IGraphe {
 		return false;
 	}
 
+	/**
+	 * @brief Ajoute un sommet au graphe si non déja présent
+	 * @param noeud : sommet à ajouter
+	 */
 	@Override
 	public void ajouterSommet(String noeud) {
 		if (!contientSommet(noeud)) {
 			arcs.add(new Arc(noeud));
 		}
 	}
-
-	private boolean noDestination(String sommet) {
+	
+	/**
+	 * @brief Renvoie un booléen indiquant si un sommet n'a pas de destination ou non
+	 * @param sommet : sommet source
+	 * @return true si le le sommet n'a pas de destination, false sinon
+	 */
+	private boolean pasDeDestination(String sommet) {
 		assert(contientSommet(sommet));
 		for (Arc arc: arcs) {
 			if (arc.getSource().equals(sommet) && arc.getDestination() == "") {
@@ -82,6 +133,14 @@ public class GrapheLArcs implements graphe.IGraphe {
 		return false;
 	}
 	
+	/**
+	 * @brief Ajoute un arc entre deux sommets, avec pour valuation une valeur donnée
+	 * @pre il ne faut pas que l'arc soit déjà présent
+	 * @pre il ne faut pas que la valeur donnée en paramètre soit négative
+	 * @param source : sommet duquel part l'arc
+	 * @param destination : sommet vers lequel arrive l'arc
+	 * @param valeur : valuation de l'arc
+	 */
 	@Override
 	public void ajouterArc(String source, String destination, Integer valeur) {
 		if (contientArc(source, destination)) {
@@ -90,12 +149,16 @@ public class GrapheLArcs implements graphe.IGraphe {
 		if (valeur < 0) {
 			throw new IllegalArgumentException("valuation negative");
 		}
-		if(contientSommet(source) && noDestination(source)) {
+		if(contientSommet(source) && pasDeDestination(source)) {
 			oterArc(source, "");
 		}
 		arcs.add(new Arc(source, destination, valeur));
 	}
 	
+	/**
+	 * @brief Permet d'enlever un sommet du graphe si présent dans ce dernier
+	 * @param noeud : sommet à supprimer
+	 */
 	@Override
 	public void oterSommet(String noeud) {
 		if (!contientSommet(noeud)) {
@@ -111,6 +174,12 @@ public class GrapheLArcs implements graphe.IGraphe {
 		arcs.removeAll(supprArc);
 	}
 
+	/**
+	 * @brief Permet d'enlever un arc entre deux sommets
+	 * @pre il faut que l'arc existe déjà
+	 * @param source : sommet duquel part l'arc
+	 * @param destination : sommet vers lequel va l'arc
+	 */
 	@Override
 	public void oterArc(String source, String destination) {
 		if (!contientArc(source, destination)) {
@@ -126,6 +195,11 @@ public class GrapheLArcs implements graphe.IGraphe {
 		arcs.remove(supprArc);
 	}
 	
+	/**
+	 * @brief Renvoie une chaîne représentant le graphe
+	 * @return chaîne représentant le graphe
+	 */
+	@Override
 	public String toString() {
 		Collections.sort(arcs);
 		String s = "";
