b2b7ed0d34c99c20e5a496504c8c9ef65a691137
ysy <128068301+ysy15@users.noreply.github.com>
2023-04-29 14:14:13 +0200

    Update GraphImporter.java

--- src/graphe/GraphImporter.java
+++ src/graphe/GraphImporter.java
@@ -1,63 +1,95 @@
 package graphe;
 
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import Arc.Arc;
 
 public class GraphImporter {
-    public static int importerReponse(String filePath, List<Integer> chemin) throws FileNotFoundException {
-        File file = new File(filePath);
-        try (Scanner sc = new Scanner(file)) {
-            sc.nextLine(); // nom de l'algo recommandé
-            // distance attendue
-            int distance = Integer.parseInt(sc.nextLine().trim());
-            // chemin
-            for (String s : sc.nextLine().split(" "))
-                chemin.add(Integer.parseInt(s) - 1);
-            return distance;
-        }
-    }
+	
+	public static int importerReponse(String nomFichier, List<Integer> listeEntiers) throws IOException {
+        int distance = -1;
 
-    public static Arc importer(String filepath, IGraphe g)  {
-        File file = new File(filepath);
-        return importer(file, g);
-    }
+        try (Scanner scanner = new Scanner(new File(nomFichier))) {
+            if (!scanner.hasNextLine()) {
+                throw new IOException("Le fichier est vide.");
+            }
+            String premiereLigne = scanner.nextLine();
 
-    private static Arc importer(File file, IGraphe g)  {
-        try (Scanner sc = new Scanner(file)) {
-            String line;
-            if (!sc.hasNextLine()) {
-                throw new IllegalArgumentException("Pas de graphe dans " + file);
+            if (premiereLigne.startsWith("pas de chemin entre")) {
+                String[] ligneDecoupee = premiereLigne.split(" ");
+                if (ligneDecoupee.length != 7) {
+                    throw new IOException("Format incorrect pour la premiere ligne.");
+                }
+                listeEntiers.add(Integer.parseInt(ligneDecoupee[4]));
+                listeEntiers.add(Integer.parseInt(ligneDecoupee[6]));
+            } else {
+                if (!scanner.hasNextLine()) {
+                    throw new IOException("Deuxieme ligne non trouvee.");
+                }
+                distance = scanner.nextInt();
+                scanner.nextLine(); 
+                if (!scanner.hasNextLine()) {
+                    throw new IOException("Troisieme ligne non trouvee.");
+                }
+                String troisiemeLigne = scanner.nextLine();
+                List<Integer> tempList = Arrays.stream(troisiemeLigne.split(" "))
+                                               .map(Integer::parseInt)
+                                               .collect(Collectors.toList());
+                listeEntiers.addAll(tempList);
             }
-            line = sc.nextLine(); // non utilisé
-            Arc a = null;
-            while (sc.hasNextLine()) {
-                line = sc.nextLine();
-                a = parse(line);
-                if (sc.hasNextLine())
-                    g.ajouterArc(a.getSource(), a.getDestination(), a.getValuation());
-            }
-            return a;
-        } catch (FileNotFoundException e) {
-            throw new IllegalArgumentException("Pas de graphe dans " + file);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("valuation incorrecte dans " + file);
+            throw new IOException("Le fichier contient des données mal formatees.", e);
         }
-    }
 
-    public static Arc parse(String string) {
-        String[] parts = string.split(" ");
-        String source;
-        int valuation;
-        String destination;
-        try {
-            source = parts[0];
-            valuation = Integer.valueOf(parts[1]);
-            destination = parts[2];
-        } catch (Exception e) {
-            throw new IllegalArgumentException(string + " n'est pas un arc");
-        }
-        return new Arc(source, destination, valuation);
+        return distance;
     }
+	
+	public static Arc importer(String filePath, IGraphe g)  {
+		File file = new File(filePath);
+		return importer(file, g);
+	}
+
+	public static Arc importer(File file, IGraphe g)  {
+		try (Scanner sc = new Scanner(file)) {
+			String line;
+			if (!sc.hasNextLine()) {
+				throw new IllegalArgumentException("Pas de graphe dans " + file);
+			}
+			line = sc.nextLine(); // nom d'algorithme non utilise
+			Arc a = null;
+			while (sc.hasNextLine()) {
+				line = sc.nextLine();
+				a = parse(line);
+				if (sc.hasNextLine())
+					g.ajouterArc(a.getSource(), a.getDestination(), a.getValuation());
+			}
+			return a;
+		} catch (FileNotFoundException e) {
+			throw new IllegalArgumentException("Pas de graphe dans " + file);
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException("valuation incorrecte dans " + file);
+		}
+	}
+
+	public static Arc parse(String string) {
+		String[] parts = string.split(" ");
+		String source;
+		int valuation;
+		String destination;
+		try {
+			source = parts[0];
+			valuation = Integer.valueOf(parts[1]);
+			destination = parts[2];
+		} catch (Exception e) {
+			throw new IllegalArgumentException(string + " n'est pas un arc");
+		}
+		return new Arc(source, destination, valuation);
+	}
 }
