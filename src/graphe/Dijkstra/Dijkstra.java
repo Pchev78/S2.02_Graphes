3db7e85a8891ac7f3350f8f9ed0f7999c4da56d3
ysy <128068301+ysy15@users.noreply.github.com>
2023-05-21 14:48:31 +0200

    Ajout Documentation

--- src/graphe/Dijkstra/Dijkstra.java
+++ src/graphe/Dijkstra/Dijkstra.java
@@ -6,18 +6,39 @@ import java.util.Map;
 
 import graphe.core.IGrapheConst;
 
+/**
+ * Classe implémentant l'algorithme de Dijkstra
+ * @author MOGNE Youssouf-A., CHEVILY Pierre, BALASSOUPRAMANIEN Madavan, BANOL MORENO Marcell 101
+ *
+ */
 public class Dijkstra{
 	
+	/**Constante représentant une valeur infinie*/
 	private final static int INFINI = Integer.MAX_VALUE-1;
 	
+	/**
+	 * Initialise l'algorithme de Dijkstra, notamment les deux dictionnaires prev et dist.
+	 * @param g graphe cible de Dijkstra
+	 * @param source sommet de départ
+	 * @param dist dictionnaire répertoriant pour chaque sommet, la plus courte distance entre ce dernier et la source
+	 * @param prev dictionnaire répertoriant pour chaque sommet son prédécesseur dans le chemin avec la plus courte distance 
+	 * entre la source et ce sommet
+	 */
     private static void initDijkstra(IGrapheConst g, String source, Map<String, Integer> dist, Map<String, String> prev) {
         for (String s : g.getSommets()) {
-            dist.put(s, INFINI);
             prev.put(s, null);
+            dist.put(s, INFINI);
         }
         dist.put(source, 0);
     }
 
+    /**
+     * Retourne le sommet ayant jusqu'ici la plus courte distance dans le dictionnaire dist 
+     * parmi les sommets qu'il reste à visiter
+     * @param sAVisiter sommets restants à visiter 
+     * @param dist dictionnaire répertoriant pour chaque sommet, la plus courte distance entre ce dernier et la source
+     * @return sommet avec la distance minimum
+     */
     private static String trvDistMin(List<String> sAVisiter, Map<String, Integer> dist){
         int distMin = Integer.MAX_VALUE;
         String sMin = null;
@@ -31,6 +52,15 @@ public class Dijkstra{
         return sMin;
     }
 
+    /**
+     * Implémente l'algorithme de Dijkstra permettant de retrouver les plus courts chemin permettant d'aller 
+     * d'un sommet source à un autredans un graphe cible
+     * @param g graphe cible de Dijkstra
+	 * @param source sommet de départ
+	 * @param dist dictionnaire répertoriant pour chaque sommet, la plus courte distance entre ce dernier et la source
+	 * @param prev dictionnaire répertoriant pour chaque sommet son prédécesseur dans le chemin avec la plus courte distance 
+	 * entre la source et ce sommet
+     */
     public static void dijkstra(IGrapheConst g, String source, Map<String, Integer> dist, Map<String, String> prev) {
 
         initDijkstra(g, source, dist, prev);
